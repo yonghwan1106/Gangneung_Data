ad4282088661b63485f160290081f6251d929b55
yonghwan1106 <sanoramyun8@gmail.com>
2024-10-04 11:40:41 +0900

    Add files via upload

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,13 +4,17 @@ import numpy as np
 def load_and_preprocess_data():
     # 데이터 로드
     climate_data = pd.read_csv('data/climate_data.csv')
+        print("Climate data columns:", climate_data.columns)
     agriculture_data = pd.read_csv('data/agriculture_data.csv')
     crop_production_data = pd.read_csv('data/crop_production_data.csv')
-    air_quality_data = pd.read_csv('data/air_quality_data.csv')
+    air_quality_data = pd.read_csv('data/air_quality_data.txt')
 
     # 데이터 전처리
     def preprocess_data(df):
-        df['Date'] = pd.to_datetime(df['Date'])
+        if 'Year' in df.columns:
+            df['Date'] = pd.to_datetime(df['Year'], format='%Y')
+        elif 'Date' not in df.columns:
+            df['Date'] = pd.to_datetime(df.index)
         df.set_index('Date', inplace=True)
         return df
 
